8679d35a4f377c5454c095489f6556259c965091
Kevin Davezac <kevindavezac@outlook.com>
2024-09-02 18:42:07 -0300

    Segundo commit, se agrega metodo get

--- src/main/java/com/MejorPrecio/MejorPrecio/Controller/MainController.java
+++ src/main/java/com/MejorPrecio/MejorPrecio/Controller/MainController.java
@@ -3,11 +3,13 @@ package com.MejorPrecio.MejorPrecio.Controller;
 import com.MejorPrecio.MejorPrecio.Model.Student;
 import com.MejorPrecio.MejorPrecio.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/student")
 public class MainController {
     @Autowired
     StudentRepository studentRepository;
@@ -16,4 +18,15 @@ public class MainController {
         public void addStudent(@RequestBody Student student) {
             studentRepository.save(student);
         }
+
+    @GetMapping("/getAllStudents")
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/{nro}")
+    public Optional<Student> getStudentById(@PathVariable Integer nro) {
+        return studentRepository.findById(nro);
+    }
+
 }
--- src/main/java/com/MejorPrecio/MejorPrecio/Repository/StudentRepository.java
+++ src/main/java/com/MejorPrecio/MejorPrecio/Repository/StudentRepository.java
@@ -3,5 +3,5 @@ package com.MejorPrecio.MejorPrecio.Repository;
 import com.MejorPrecio.MejorPrecio.Model.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface StudentRepository extends MongoRepository<Student, String> {
+public interface StudentRepository extends MongoRepository<Student, Integer> {
 }
